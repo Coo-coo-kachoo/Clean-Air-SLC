50e99cb3f6a1f369d5270dcb2f5aa8dbab062116
Allen Eames <alleneames@icloud.com>
2017-08-14 14:51:50 -0600

    updated styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./navbar/navbar.css";
 
 import Navbar from "./navbar/nav.js";
 import LandingContainer from "./landing/landing-container.js";
