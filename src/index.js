6b6b4331d24f6908dc9c51f0fd0379582941c67f
Allen Eames <alleneames@icloud.com>
2017-08-14 13:53:43 -0600

    temp commit

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
         return (
             <div>
                 <Navbar />
-                <LandingContainer/>
+                <LandingContainer />
             </div>
         );
     }
