afd33c0759a5b14dd75c1ba29bb43c5114c69643
Sarah Baldwin <sarahb1245@gmail.com>
2017-08-14 13:42:50 -0600

    base server to use with api

---
+++ server/index.js
@@ -0,0 +1,35 @@
+//framework for node.js
+const express = require('express');
+// Cross-Origin Resource Sharing (CORS). CORS introduces a standard mechanism that can be used by all browsers for implementing cross-domain requests.
+// The spec defines a set of headers that allow the browser and server to communicate about which requests are (and are not) allowed.
+// CORS continues the spirit of the open web by bringing API access to all.
+const cors = require('cors');
+//Node.js body parsing middleware.
+//Parses incoming request bodies in a middleware before your handlers, available under the req.body property.
+const bodyParser = require('body-parser');
+//Mongoose provides a schema-based solution to model your application data.
+//It includes built-in type casting, validation, query building, business logic hooks and more.
+const mongoose = require('mongoose');
+
+//keeping these settings (db name, port, and secret) seperate makes it easier to change once you hit deployment
+let settings = require('./config/settings.js');
+
+//import routes
+let airQualityRouter = require('./routes/air-quality.js');
+
+let PORT = process.env.PORT || settings.port;
+
+mongoose.connect(`mongodb://localhost:27017/${settings.db}`);
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({encoded:false}));
+app.use(bodyParser.json());
+
+//app.use routers
+app.use('/airquality', airQualityRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
---
+++ server/routes/air-quality.js
@@ -0,0 +1,16 @@
+let request = require('request');
+
+app.get("/airquality/:zip", (req, res) => {
+  let today = new Date();
+  let date = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+  request(`http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=${req.params.zip}&date=${date}&distance=25&API_KEY=3CF907AB-9ECF-459C-A9B3-1746C8A98DD3`, (err, response, body) => {
+    if(err) {
+      res.status(500).send({"message":"ERROR within server", err});
+    } else {
+      res.status(200).send({"message":"SUCCESS data returned", data: JSON.parse(body)})
+    }
+  });
+});
+
+
+module.exports = airQualityRouter;
